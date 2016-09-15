ddcedcf5697602262fc78ba1fd3aff9cc8b8bb6b
marek.laskowski <marek.laskowski@senecacollegeca>
2016-09-15 16:09:37 -0400

    added the second activty with butttons and stuff

--- app/src/main/java/com/example/marek/activitiesandintents/MainActivity.java
+++ app/src/main/java/com/example/marek/activitiesandintents/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //once the activity is created let's assign some code to the Button when it is clicked
-        Button msgButton = (Button)findViewById(R.id.messageButton);
+        Button msgButton = (Button)findViewById(R.id.a2_button);
         //assign code to the onClickListener by defining
         //an anonymous subclass of OnClickListener and overriding its onClickMethod
         //notice here we are calling new and passing the created OnClickListener to the "setOnClickListener" method instead of storing the reference as a variable
@@ -23,10 +23,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 //in this example we will use the button to send a message
                 //let's create an intent
-                Intent msg_intent = new Intent(Intent.ACTION_SEND);
+                Intent msg_intent = new Intent("com.seneca.lab2b.marek");
                 //add some extra information to the intent
-                msg_intent.putExtra(Intent.EXTRA_TEXT, "This is my message blah blah blah");
-                msg_intent.setType("text/plain"); //describes the MIME type of the content
+                //msg_intent.putExtra(Intent.EXTRA_TEXT, "This is my message blah blah blah");
+                //msg_intent.setType("text/plain"); //describes the MIME type of the content
                 //fire off the intent
                 startActivity(msg_intent);
             }
